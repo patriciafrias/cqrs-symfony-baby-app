33d912eb40ebe2f0b3951ed6deb2b4f5db5a7db0
Patricia Frias <patriciafm4@gmail.com>
2021-02-09 09:28:33 +0100

    Add symfony form

---
+++ src/Form/MilestoneType.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Domain\Milestone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MilestoneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('height', NumberType::class, [
+                'required' => true,
+            ])
+            ->add('moment', DateTimeType::HTML5_FORMAT, [
+                'required' => true,
+                ]
+            )
+            ->add('save', SubmitType::class
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Milestone::class,
+        ]);
+    }
+}
