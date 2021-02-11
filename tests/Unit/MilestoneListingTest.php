ca5308076585afeaaa9f20a8e76706517bc49540
Patricia Frias <patriciafm4@gmail.com>
2021-02-11 11:36:41 +0100

    Add Domain unit test (TDD)

---
+++ tests/Unit/MilestoneListingTest.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Unit;
+
+use App\Domain\Height;
+use App\Domain\Milestone;
+use App\Domain\MilestoneRepositoryInterface;
+use App\Infrastructure\Persistence\MilestoneRepositoryInMemory;
+use PHPUnit\Framework\TestCase;
+
+class MilestoneListingTest extends TestCase
+{
+    private ?MilestoneRepositoryInterface $milestoneRepository;
+
+    /**
+     * @test
+     */
+    public function milestoneList_returnsAllMilestones()
+    {
+        $milestoneRepository = new MilestoneRepositoryInMemory([
+            new Milestone(Height::create(51), new \DateTime('now')),
+            new Milestone(Height::create(52), new \DateTime('now')),
+        ]);
+
+        $milestoneList = $milestoneRepository->findAll();
+
+        $this->assertCount(2, $milestoneList);
+    }
+}
